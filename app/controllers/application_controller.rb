4f9d74073913db3a053e679886a9c17c5f02e875
EC2 Default User <ec2-user@ip-172-31-8-23.ap-northeast-1.compute.internal>
2023-03-04 08:58:29 +0000

    [Add]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
-  # サインイン後の遷移先→about
+
   def after_sign_in_path_for(resource)
-    about_path
+    books_path
   end
-  # サインアウト後の遷移先→about
+
   def after_sign_out_path_for(resource)
     about_path
   end
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+
+  def index
+   @book = Book.new
+   @books = Book.all
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def dastroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+ 
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+end
