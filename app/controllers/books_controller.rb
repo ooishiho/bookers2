6e61a576787c6f4093ea6dcec42eddc76102d322
ooishiho <yuidacheng697@gmail.com>
2023-03-14 04:12:17 +0000

    [add]book edit bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,7 +31,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
    if @book.update(book_params)
-    flach[:notice] = "You have updated book successfully."
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id)
    else
     render :edit
